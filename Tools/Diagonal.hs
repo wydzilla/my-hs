48d9d7a715717ea7926145d403d431d9c17b06d1
Your Name <wydmansk@tlen.pl>
2012-03-09 17:22:47 +0100

    hmmm

--- Tools/Diagonal.hs
+++ Tools/Diagonal.hs
@@ -1,15 +1,16 @@
+module Diagonal where
+
 genFromPair (e1, e2) = diagonal [[[ x*e1 , y*e2 ] | x <- [0..]] | y <- [0..]]
 
 diagonal :: [[a]] -> [a]
 diagonal = concat . stripe
+where
+    stripe [] = []
+    stripe ([]:xss) = stripe xss
+    stripe ((x:xs):xss) = [x] : zipCons xs (stripe xss)
 
-stripe [] = []
-stripe ([]:xss) = stripe xss
-stripe ((x:xs):xss) = [x] : zipCons xs (stripe xss)
-
-zipCons [] ys = ys
-zipCons xs [] = map (:[]) xs
-zipCons (x:xs) (y:ys) = (x:y) : zipCons xs ys
+    zipCons [] ys = ys
+    zipCons xs [] = map (:[]) xs
+    zipCons (x:xs) (y:ys) = (x:y) : zipCons xs ys
 
 
-zipCons2 xs ys= map (\(x,y)) $ zip xs ys
