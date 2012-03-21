55f7d0f3f9f102e1c721ebff5bef13ff6a0c5654
wydzilla <wydmansk@tlen.pl>
2012-03-21 23:04:44 +0100

    p 43 cleaned up

--- Done/p43_pandigit_substring.hs
+++ Done/p43_pandigit_substring.hs
@@ -12,6 +12,12 @@ test7 = divisibleBy 17
 divisibleBy n xs = (fromDigits xs) `mod` n == 0
 
 subList startIndex xs = take 3 $ drop startIndex xs
-test  xs = (test1  $ subList 1 xs) && (test2  $ subList 2 xs) && (test3  $ subList 3 xs) && (test4  $ subList 4 xs) && (test5  $ subList 5 xs) && (test6  $ subList 6 xs) && (test7  $ subList 7 xs)
+test  xs = (test1  $ subList 1 xs) && 
+	(test2  $ subList 2 xs) && 
+	(test3  $ subList 3 xs) && 
+	(test4  $ subList 4 xs) && 
+	(test5  $ subList 5 xs) && (test6  $ subList 6 xs) && (test7  $ subList 7 xs)
 
-result = filter test $ permutations [0..9]
+result' = filter test $ permutations [0..9]
+
+result = sum $ map fromDigits result'
